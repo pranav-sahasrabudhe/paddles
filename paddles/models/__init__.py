1efbbfeb2670ce725003418ed02ef44409a420b4
Aishwarya Mathuria <amathuri@redhat.com>
2021-06-11 15:10:13 +0530

    Fixing unit test failures with SQLite: 1. listeners option in configuration is now deprecated. Using event.listen instead. 2. Adding alternative to using Sequence() since it is not supported in SQLite

--- paddles/models/__init__.py
+++ paddles/models/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, MetaData, event
 from sqlalchemy.orm import scoped_session, sessionmaker, object_session, mapper
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.pool import Pool
 from pecan import conf
 
 
@@ -72,17 +73,20 @@ def init_model():
 
     """
     conf.sqlalchemy.engine = _engine_from_config(conf.sqlalchemy)
+    config = dict(conf.sqlalchemy)
+    if 'sqlite' in config['url']:
+        event.listen(Pool, 'connect', sqlite_connect, named=True)
 
-class SqliteListener():
-    def connect(self, dbapi_con, con_record):
-        dbapi_con.execute('PRAGMA journal_mode=MEMORY')
-        dbapi_con.execute('PRAGMA synchronous=OFF')
+
+def sqlite_connect(**kw):
+    dbapi_con = kw['dbapi_connection']
+    dbapi_con.execute('PRAGMA journal_mode=MEMORY')
+    dbapi_con.execute('PRAGMA synchronous=OFF')
+
 
 def _engine_from_config(configuration):
     configuration = dict(configuration)
     url = configuration.pop('url')
-    if 'sqlite' in url:
-        configuration['listeners'] = [SqliteListener()]
     return create_engine(url, **configuration)
 
 
@@ -121,4 +125,4 @@ def flush():
 from .runs import Run  # noqa
 from .jobs import Job  # noqa
 from .nodes import Node  # noqa
-from .queue import Queue
+from .queue import Queue  #noqa
--- paddles/tests/controllers/test_runs.py
+++ paddles/tests/controllers/test_runs.py
@@ -37,7 +37,6 @@ class TestRunController(TestApp):
     def test_allows_waiting_status(self):
         self.app.post_json('/runs/', dict(name="foo"))
         self.app.post_json('/runs/foo/jobs/', dict(
-            job_id=1,
             status="waiting",
         ))
         new_run = Run.get(1)
@@ -46,11 +45,9 @@ class TestRunController(TestApp):
     def test_running_status_with_waiting_and_running_jobs(self):
         self.app.post_json('/runs/', dict(name="foo"))
         self.app.post_json('/runs/foo/jobs/', dict(
-            job_id=1,
             status="running",
         ))
         self.app.post_json('/runs/foo/jobs/', dict(
-            job_id=2,
             status="waiting",
         ))
         new_run = Run.get(1)
@@ -71,7 +68,7 @@ class TestRunController(TestApp):
     def test_create_new_job(self):
         self.app.post_json('/runs/', dict(name="foo"))
         self.app.post_json('/runs/foo/jobs/', dict(
-            job_id=1,
+            status='queued',
         ))
         new_job = Job.get(1)
         assert new_job.job_id == '1'
@@ -84,7 +81,7 @@ class TestRunController(TestApp):
 
     def test_delete_full_run(self):
         self.app.post_json('/runs/', dict(name='foo'))
-        self.app.post_json('/runs/foo/', dict(job_id='42'))
+        self.app.post_json('/runs/foo/', dict(user='test'))
         self.app.post_json('/runs/foo/', dict(job_id='9'))
         self.app.post_json('/runs/foo/', dict(job_id='12345'))
         self.app.delete('/runs/foo/')
@@ -205,10 +202,8 @@ class TestRunController(TestApp):
         run_names = ['run_one', 'run_two']
         for run_name in run_names:
             self.app.post_json('/runs/', dict(name=run_name))
-            self.app.post_json('/runs/%s/jobs/' % run_name, dict(job_id=1,
-                                                            status='queued'))
-            self.app.post_json('/runs/%s/jobs/' % run_name, dict(job_id=2,
-                                                            status='queued'))
+            self.app.post_json('/runs/%s/jobs/' % run_name, dict(status='queued'))
+            self.app.post_json('/runs/%s/jobs/' % run_name, dict(status='queued'))
 
         result = self.app.get('/runs/queued/').json
         result_names = sorted([r['name'] for r in result])
@@ -218,10 +213,8 @@ class TestRunController(TestApp):
         run_names = ['run_one', 'run_two']
         for run_name in run_names:
             self.app.post_json('/runs/', dict(name=run_name))
-            self.app.post_json('/runs/%s/jobs/' % run_name, dict(job_id=1,
-                                                            status='queued'))
-            self.app.post_json('/runs/%s/jobs/' % run_name, dict(job_id=2,
-                                                            status='dead'))
+            self.app.post_json('/runs/%s/jobs/' % run_name, dict(status='queued'))
+            self.app.post_json('/runs/%s/jobs/' % run_name, dict(status='dead'))
 
         result = self.app.get('/runs/queued/').json
         result_names = sorted([r['name'] for r in result])
